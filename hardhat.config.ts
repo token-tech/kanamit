e77552eb33bb43e1175132d722941aabf4a9f7fd
zhoushx1018@gmail.com <zhoushx1018@gmail.com>
2021-05-17 22:07:17 +0800

    new deploy config

---
+++ hardhat.config.ts
@@ -0,0 +1,14 @@
+import {HardhatUserConfig} from 'hardhat/types';
+import 'hardhat-deploy';
+import 'hardhat-deploy-ethers';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.6.12',
+  },
+  namedAccounts: {
+    deployer: 0,
+    tokenOwner: 1,
+  },
+};
+export default config;
