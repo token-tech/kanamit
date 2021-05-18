69d16178d79ce3d91ceabeb4562ad79455a13c79
zhoushx1018@gmail.com <zhoushx1018@gmail.com>
2021-05-18 21:07:20 +0800

    unit test, KanamitTrade deposit

--- test/KatamitTrade.test.ts
+++ test/KatamitTrade.test.ts
@@ -15,4 +15,34 @@ describe("total supply", function () {
 
     expect(ownerBalance).to.equal(supply);
   });
+
+  it("转币测试", async function () {
+    await deployments.fixture(["KanamitTrade"]);
+    const { tokenOwner, user0 } = await getNamedAccounts();
+    const KanamitTrade = await ethers.getContract("KanamitTrade");
+    let ownerBalance = await KanamitTrade.balanceOf(tokenOwner);
+    let supply = await KanamitTrade.totalSupply();
+
+    console.log('ownerBalance', ownerBalance.toNumber());
+    console.log('supply', supply.toNumber());
+
+    expect(ownerBalance).to.equal(supply);
+
+    // KanamitTrade.deposit({value: 100000000000});
+    KanamitTrade.deposit({ value: ethers.utils.parseEther("0.0000012345") });
+    
+    const user0Balance = await KanamitTrade.balanceOf(user0);
+    console.log('user0Balance', user0Balance.toNumber());
+
+    ownerBalance = await KanamitTrade.balanceOf(tokenOwner);
+    console.log('ownerBalance', ownerBalance.toNumber());
+
+    supply = await KanamitTrade.totalSupply();
+    console.log('supply', supply.toNumber());
+
+  });
+
 });
+
+
+ 
